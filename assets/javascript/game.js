d26ebd4d3718a703060e6ac370fa4b36545c64db
Noel Cruz <noel.j.cruz@outlook.com>
2019-06-24 23:39:57 -0700

    Finished the Psychic Game. Included comments to explain the JS code. Made sure everything rendered properly in the browser.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,44 +7,49 @@ let Letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 
 let Wins = 0;
 let Losses = 0;
-let GuessesSoFar = "";
 let GuessesLeft = 9;
+let GuessesSoFar = "";
+let winsText = document.getElementById("wins-text");
+let lossesText = document.getElementById("losses-text");
+let leftText = document.getElementById("left-text");
+let sofarText = document.getElementById("sofar-text");
 
 // Starts off the game my having the Computer select a random letter from the Letters array.
 
-let CompLetter = Letters[Math.floor(Math.random()*Letters.length)];
-console.log(CompLetter);
+let CompLetter = Letters[Math.floor(Math.random() * Letters.length)];
 
 document.onkeyup = function (event) {
-
-// Upon pressing a key, the following happens: That key is logged as the user's guess, it is added to the list of guesses so far, and the number of guesses left is reduced by one.
-
+    
+    // Upon pressing a key, the following happens: That key is logged as the user's guess, the number of guesses left is reduced by one, and the guess is added to the list of guesses so far.
+    
     let UserGuess = event.key;
-    console.log(UserGuess);
-    GuessesSoFar = GuessesSoFar + UserGuess;
     GuessesLeft--;
-    console.log(GuessesSoFar);
-
-// If the user's guess matches the computer's letter, then a win is added, the guesses so far resets to blank, the guesses left resets to 9, and the computer selects a new letter at random.
-
-    if (UserGuess === CompLetter) { 
+    GuessesSoFar = GuessesSoFar + UserGuess;
+    
+    
+    // If the user's guess matches the computer's letter, then a win is added, the guesses left resets to 9, the guesses so far resets to blank, and the computer selects a new letter at random.
+    
+    if (UserGuess === CompLetter) {
         Wins++;
-        GuessesSoFar = "";
         GuessesLeft = 9;
-        CompLetter = Letters[Math.floor(Math.random()*Letters.length)];
-        console.log(CompLetter);
+        GuessesSoFar = "";
+        CompLetter = Letters[Math.floor(Math.random() * Letters.length)];
     }
-
-// If the user runs out of guesses, then a loss is added, the guesses so far resets to blank, the guesses left resets to 9, and the computer selects a new letter at random.
+    
+    // If the user runs out of guesses, then a loss is added, the guesses left resets to 9, the guesses so far resets to blank, and the computer selects a new letter at random.
     
     if (GuessesLeft < 1) {
         Losses++;
-        GuessesSoFar = "";
         GuessesLeft = 9;
-        CompLetter = Letters[Math.floor(Math.random()*Letters.length)];
-        console.log(CompLetter);
+        GuessesSoFar = "";
+        CompLetter = Letters[Math.floor(Math.random() * Letters.length)];
     }
     
-    console.log(GuessesLeft);
-
+    // Displays the changes in the game with each key stroke.
+    
+    winsText.textContent = "Wins: " + Wins;
+    lossesText.textContent = "Losses: " + Losses;
+    leftText.textContent = "Guesses Left: " + GuessesLeft;
+    sofarText.textContent = "Your Guesses so far: " + GuessesSoFar;
+    
 } 
