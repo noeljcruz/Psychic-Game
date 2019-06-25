056983d06be8192a8bfb99ae50032b9c2478c055
Noel Cruz <noel.j.cruz@outlook.com>
2019-06-24 23:06:36 -0700

    Completed a good portion of the JS code. Did not use any loops for the logic. Instead, had a few things happen each time a key was pressed and inclued two 'if' statements for when the user guessed correctly or ran out of guesses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,50 @@
 
 // Array that contains every letter in the alphabet.
+
 let Letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Initial variable declarations.
 
 let Wins = 0;
 let Losses = 0;
-let GuessesLeft = 9;
 let GuessesSoFar = "";
+let GuessesLeft = 9;
 
+// Starts off the game my having the Computer select a random letter from the Letters array.
 
 let CompLetter = Letters[Math.floor(Math.random()*Letters.length)];
 console.log(CompLetter);
 
-
 document.onkeyup = function (event) {
+
+// Upon pressing a key, the following happens: That key is logged as the user's guess, it is added to the list of guesses so far, and the number of guesses left is reduced by one.
 
     let UserGuess = event.key;
     console.log(UserGuess);
+    GuessesSoFar = GuessesSoFar + UserGuess;
     GuessesLeft--;
+    console.log(GuessesSoFar);
 
-    
-    if (UserGuess === CompLetter) {
-        
+// If the user's guess matches the computer's letter, then a win is added, the guesses so far resets to blank, the guesses left resets to 9, and the computer selects a new letter at random.
+
+    if (UserGuess === CompLetter) { 
+        Wins++;
+        GuessesSoFar = "";
         GuessesLeft = 9;
         CompLetter = Letters[Math.floor(Math.random()*Letters.length)];
         console.log(CompLetter);
-        
+    }
+
+// If the user runs out of guesses, then a loss is added, the guesses so far resets to blank, the guesses left resets to 9, and the computer selects a new letter at random.
+    
+    if (GuessesLeft < 1) {
+        Losses++;
+        GuessesSoFar = "";
+        GuessesLeft = 9;
+        CompLetter = Letters[Math.floor(Math.random()*Letters.length)];
+        console.log(CompLetter);
     }
     
     console.log(GuessesLeft);
-
-    // if (UserGuess !== CompLetter) {
-    //     GuessesLeft--;
-    //     console.log(GuessesLeft);
-    // }
-    // else if (GuessesLeft < 1) {
-    //     let CompLetter = Letters[Math.floor(Math.random()*Letters.length)];
-    //     console.log(CompLetter);
-    //     let GuessesLeft = 9;
-
-    // }
-
-    // if (UserGuess !== CompLetter) {
-    //     GuessesLeft--;
-    // }
-    // else {
-    //     GuessesLeft = 9;
-    //     CompLetter = Letters[Math.floor(Math.random() * Letters.length)];
-    // }
-    // console.log(GuessesLeft);
-    // console.log(CompLetter);
 
 } 
