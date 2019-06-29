de86c2e4a774e34a4ecedfc00f71f38233ebe974
Noel Cruz <noel.j.cruz@outlook.com>
2019-06-29 14:35:07 -0700

    Created a new function that resets the computer choice, sets guesses left to 9, and guesses so far blank.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,18 +5,25 @@ let Letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 
 // Initial variable declarations.
 
-let Wins = 0;
-let Losses = 0;
-let GuessesLeft = 9;
-let GuessesSoFar = "";
 let winsText = document.getElementById("wins-text");
 let lossesText = document.getElementById("losses-text");
 let leftText = document.getElementById("left-text");
 let sofarText = document.getElementById("sofar-text");
 
+let Wins = 0;
+let Losses = 0;
+let GuessesLeft;
+let GuessesSoFar;
+
 // Starts off the game my having the Computer select a random letter from the Letters array.
 
-let CompLetter = Letters[Math.floor(Math.random() * Letters.length)];
+function NewLetter () {
+    GuessesLeft = 9
+    GuessesSoFar = "";
+    CompLetter = Letters[Math.floor(Math.random() * Letters.length)];
+}
+
+NewLetter();
 
 document.onkeyup = function (event) {
     
@@ -26,27 +33,22 @@ document.onkeyup = function (event) {
     GuessesLeft--;
     GuessesSoFar = GuessesSoFar + UserGuess;
     
-    
     // If the user's guess matches the computer's letter, then a win is added, the guesses left resets to 9, the guesses so far resets to blank, and the computer selects a new letter at random.
     
     if (UserGuess === CompLetter) {
         Wins++;
-        GuessesLeft = 9;
-        GuessesSoFar = "";
-        CompLetter = Letters[Math.floor(Math.random() * Letters.length)];
+        NewLetter();
     }
     
     // If the user runs out of guesses, then a loss is added, the guesses left resets to 9, the guesses so far resets to blank, and the computer selects a new letter at random.
     
     if (GuessesLeft < 1) {
         Losses++;
-        GuessesLeft = 9;
-        GuessesSoFar = "";
-        CompLetter = Letters[Math.floor(Math.random() * Letters.length)];
+        NewLetter();
     }
     
     // Displays the changes in the game with each key stroke.
-    
+
     winsText.textContent = "Wins: " + Wins;
     lossesText.textContent = "Losses: " + Losses;
     leftText.textContent = "Guesses Left: " + GuessesLeft;
